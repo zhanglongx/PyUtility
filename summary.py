dffea0cbeffa4a44db834b0c19161fc4d8ed355b
Longxiao Zhang <zhanglongx@gmail.com>
2020-06-04 11:31:01 +0800

    summary.py: default to Dropbox

--- summary.py
+++ summary.py
@@ -19,7 +19,7 @@ TITLE='summary'
 STICKY_FULL = tk.N + tk.S + tk.W + tk.E 
 PAD_DEFAULT = 5
 
-PATH='d:\\Users\\zhlx\\Documents\\stuff\\7.部门工作\\2019'
+PATH='d:\\Users\\zhlx\\Dropbox\\Work\\Administrant\\7.部门工作\\2020'
 LISTS=('考核等级', '考核分数')
 
 class summary(tk.Frame):
